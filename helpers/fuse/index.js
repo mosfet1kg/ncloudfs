3edb21be10ce48c29e99ad575301d6345d7efb6c
mosfet1kg <mosfet1kg@gmail.com>
2018-01-06 21:47:40 +0900

    test 2

--- helpers/fuse/index.js
+++ helpers/fuse/index.js
@@ -10,51 +10,118 @@ module.exports = ({ mountPath }) => {
       fuse.mount(mountPath, {
         readdir: function (path, cb) {
           console.log('readdir(%s)', path)
-          if (path === '/') return cb(0, ['helloworld.txt'])
+          if (path === '/') return cb(0, ['helloworld.txt']);
           cb(0)
         },
         getattr: function (path, cb) {
           console.log('getattr(%s)', path)
-          if (path === '/') {
-            cb(0, {
-              mtime: new Date(),
-              atime: new Date(),
-              ctime: new Date(),
-              nlink: 1,
-              size: 100,
-              mode: 16877,
-              uid: process.getuid ? process.getuid() : 0,
-              gid: process.getgid ? process.getgid() : 0
-            })
-            return
-          }
+          // if (path === '/') {
+          //   cb(0, {
+          //     mtime: new Date(),
+          //     atime: new Date(),
+          //     ctime: new Date(),
+          //     nlink: 1,
+          //     size: 100,
+          //     mode: 16877,
+          //     uid: process.getuid ? process.getuid() : 0,
+          //     gid: process.getgid ? process.getgid() : 0
+          //   })
+          //   return
+          // }
+          //
+          // if (path === '/helloworld.txt') {
+          //   cb(0, {
+          //     mtime: new Date(),
+          //     atime: new Date(),
+          //     ctime: new Date(),
+          //     nlink: 1,
+          //     size: 12,
+          //     mode: 33188,
+          //     uid: process.getuid ? process.getuid() : 0,
+          //     gid: process.getgid ? process.getgid() : 0
+          //   })
+          //   return
+          // }
+          //
+          // cb(fuse.ENOENT)
 
-          if (path === '/helloworld.txt') {
-            cb(0, {
-              mtime: new Date(),
-              atime: new Date(),
-              ctime: new Date(),
-              nlink: 1,
-              size: 12,
-              mode: 33188,
-              uid: process.getuid ? process.getuid() : 0,
-              gid: process.getgid ? process.getgid() : 0
-            })
-            return
-          }
+          ncloudFs.getFileAttr({ container: 'helloworld1', key: path }, (error, response) => {
+            // if ( error ) {
+            //   return cb(fuse.ENOENT);
+            // }
+            //
+            // const { ['last-modified']: lastModified, ['resource-type']: resourceType, size} = response;
+            // const metaData = {
+            //   // mtime: new Date(parseInt( lastModified )),
+            //   // atime: new Date(parseInt( lastModified )),
+            //   // ctime: new Date(parseInt( lastModified )),
+            //   mtime: new Date(),
+            //   atime: new Date(),
+            //   ctime: new Date(),
+            //   nlink: 1,
+            //   size: parseInt(size),
+            //   mode: (resourceType.toString() === '1' || resourceType.toString() === '2') ? permission.FOLDER : permission.REG_FILE,
+            //   uid: process.getuid ? process.getuid() : 0,
+            //   gid: process.getgid ? process.getgid() : 0
+            // };
+            // console.log( metaData );
+            // cb(0, metaData);
 
-          cb(fuse.ENOENT)
+            if (path === '/') {
+              cb(0, {
+                mtime: new Date(),
+                atime: new Date(),
+                ctime: new Date(),
+                nlink: 1,
+                size: 100,
+                mode: 16877,
+                uid: process.getuid ? process.getuid() : 0,
+                gid: process.getgid ? process.getgid() : 0
+              })
+              return
+            }
+
+            if (path === '/helloworld.txt') {
+              cb(0, {
+                mtime: new Date(),
+                atime: new Date(),
+                ctime: new Date(),
+                nlink: 1,
+                size: 12,
+                mode: 33188,
+                uid: process.getuid ? process.getuid() : 0,
+                gid: process.getgid ? process.getgid() : 0
+              })
+              return
+            }
+
+            cb(fuse.ENOENT)
+          });
+
         },
         open: function (path, flags, cb) {
           console.log('open(%s, %d)', path, flags)
           cb(0, 42) // 42 is an fd
         },
         read: function (path, fd, buf, len, pos, cb) {
-          console.log('read(%s, %d, %d, %d)', path, fd, len, pos)
-          var str = 'hello world\n'.slice(pos, pos + len)
-          if (!str) return cb(0)
-          buf.write(str)
-          return cb(str.length)
+          console.log('read(%s, %d, %d, %d)', path, fd, len, pos);
+          ncloudFs.readFile({ container:'helloworld1', key: path, pos, len}, (error, response) => {
+            if ( error ) {
+              console.log( error );
+              return cb(0);
+            }
+
+            const { data, length } = response;
+
+            if ( length === 0 ) {
+              return cb(0);
+            }
+
+            buf.write(data);
+            return cb( parseInt(length) );
+          });
+
+
         },
         // write(path, fd, buffer, length, position, cb) {
         //   console.log( path, fd);
